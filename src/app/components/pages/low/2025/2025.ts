5634f1903001ee5723277724e303ba1c828d18fc
iysaofficial <youngscientist.iysa@gmail.com>
2025-08-07 08:52:33 +0700

    LoW 2025 online -Alif

---
+++ src/app/components/pages/low/2025/2025.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-news",
+    templateUrl: "./kategori25.html",
+    styleUrls: ["./2025.scss"],
+})
+export class Kategori25 implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
+
+// Jenjang Offline
+@Component({
+    selector: "app-news",
+    templateUrl: "./jenjangofl25.html",
+    styleUrls: ["./2025.scss"],
+})
+export class Jenjangofl25 implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
+
+// Jenjang Online
+@Component({
+    selector: "app-news",
+    templateUrl: "./jenjangonl25.html",
+    styleUrls: ["./2025.scss"],
+})
+export class Jenjangonl25 implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
